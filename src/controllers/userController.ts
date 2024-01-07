d60b841f982f87356f0db0207a714b25e9e10de3
Samuel Borraccini <samuel@MacBook-Air-de-Samuel.local>
2024-01-07 16:01:14 -0300

    basic error handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,79 +5,99 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 class UserController {
   async getAll(req: Request, res: Response): Promise<void> {
-    const user: User[] = await prisma.users.findMany();
-    res.json(user);
+    try {
+      const user: User[] = await prisma.users.findMany();
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ message: "internal server error" });
+    }
   }
 
   async createUser(req: Request, res: Response): Promise<void> {
-    const bodyUser: userRequest = req.body;
-    if (!bodyUser.username || !bodyUser.password) {
-      res.status(400).json({ message: "bad body" });
-      return;
+    try {
+      const bodyUser: userRequest = req.body;
+      if (!bodyUser.username || !bodyUser.password) {
+        res.status(400).json({ message: "bad body" });
+        return;
+      }
+      const hashedPassword = await bcrypt.hash(bodyUser.password, 10);
+      const user: User = await prisma.users.create({
+        data: { username: bodyUser.username, password: hashedPassword },
+      });
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
+      res.json({ user, token });
+    } catch (error) {
+      res.status(400).json({ message: "internal server error" });
     }
-    const hashedPassword = await bcrypt.hash(bodyUser.password, 10);
-    const user: User = await prisma.users.create({
-      data: { username: bodyUser.username, password: hashedPassword },
-    });
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
-    res.json({ user, token });
   }
   async updateUser(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    const data = req.body;
-    if (!id) {
-      res.status(400).json({ message: "bad body" });
-      return;
+    try {
+      const { id } = req.params;
+      const data = req.body;
+      if (!id) {
+        res.status(400).json({ message: "bad body" });
+        return;
+      }
+      if (data.password) {
+        data.password = await bcrypt.hash(data.password, 10);
+      }
+      const user: User = await prisma.users.update({
+        where: { id: parseInt(id) },
+        data: data,
+      });
+      if (!user) {
+        res.status(401).json({ message: "user not found" });
+        return;
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(400).json({ message: "internal server error" });
     }
-    if (data.password) {
-      data.password = await bcrypt.hash(data.password, 10);
-    }
-    const user: User = await prisma.users.update({
-      where: { id: parseInt(id) },
-      data: data,
-    });
-    if (!user) {
-      res.status(401).json({ message: "user not found" });
-      return;
-    }
-    res.json(user);
   }
   async removeUser(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    if (!id) {
-      res.status(400).json({ message: "bad body" });
-      return;
+    try {
+      const { id } = req.params;
+      if (!id) {
+        res.status(400).json({ message: "bad body" });
+        return;
+      }
+      const user: User = await prisma.users.delete({
+        where: { id: parseInt(id) },
+      });
+      if (!user) {
+        res.status(401).json({ message: "user not found" });
+        return;
+      }
+      res.status(204).json(user);
+    } catch (error) {
+      res.status(400).json({ message: "internal server error" });
     }
-    const user: User = await prisma.users.delete({
-      where: { id: parseInt(id) },
-    });
-    if (!user) {
-      res.status(401).json({ message: "user not found" });
-      return;
-    }
-    res.status(204).json(user);
   }
 
   async loginUser(req: Request, res: Response): Promise<void> {
-    const userData = req.body;
-    const user = await prisma.users.findUnique({
-      where: { username: userData.username },
-    });
-    if (!user) {
-      res.status(401).json({ message: "user not found" });
-      return;
-    }
+    try {
+      const userData = req.body;
+      const user = await prisma.users.findUnique({
+        where: { username: userData.username },
+      });
+      if (!user) {
+        res.status(401).json({ message: "user not found" });
+        return;
+      }
 
-    const passwordMatch = await bcrypt.compare(
-      userData.password,
-      user.password
-    );
-    if (!passwordMatch) {
-      res.status(401).json({ message: "incorrect password" });
-      return;
+      const passwordMatch = await bcrypt.compare(
+        userData.password,
+        user.password
+      );
+      if (!passwordMatch) {
+        res.status(401).json({ message: "incorrect password" });
+        return;
+      }
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
+      res.json({ user, token });
+    } catch (error) {
+      res.status(400).json({ message: "internal server error" });
     }
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
-    res.json({ user, token });
   }
 }
 
